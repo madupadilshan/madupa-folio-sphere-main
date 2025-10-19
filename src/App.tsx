221939f66d6d63d25675dd84fdb4aa1681587328
Madupa Dilshan <madupadilshan111@gmail.com>
2025-10-19 12:14:04 +0530

    Fix router basename for GitHub Pages deployment

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/madupa-folio-sphere-main">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
